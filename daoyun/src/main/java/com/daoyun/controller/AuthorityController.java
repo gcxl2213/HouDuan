7765bf0e290fbaebc776a7090f837c6f749aeac5
gcxl2213 <81565565+gcxl2213@users.noreply.github.com>
2021-04-06 12:25:08 +0800

    Create AuthorityController.java

---
+++ daoyun/src/main/java/com/daoyun/controller/AuthorityController.java
@@ -0,0 +1,21 @@
+package com.daoyun.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 权限表：数字编号分别对应其中一种后台操作权限 前端控制器
+ * </p>
+ *
+ * @author 蔡启铨
+ * @since 2021-04-06
+ */
+@Controller
+@RequestMapping("/authority")
+public class AuthorityController {
+
+}
+
