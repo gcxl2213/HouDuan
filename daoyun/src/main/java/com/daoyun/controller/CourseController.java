2d42b67a68adb9d35b53c84200981983e85a8dfe
gcxl2213 <81565565+gcxl2213@users.noreply.github.com>
2021-04-06 12:25:12 +0800

    Create CourseController.java

---
+++ daoyun/src/main/java/com/daoyun/controller/CourseController.java
@@ -0,0 +1,21 @@
+package com.daoyun.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 课程信息表：存储课程的名字，上课老师和学校学院学期等 前端控制器
+ * </p>
+ *
+ * @author 蔡启铨
+ * @since 2021-04-06
+ */
+@Controller
+@RequestMapping("/course")
+public class CourseController {
+
+}
+
